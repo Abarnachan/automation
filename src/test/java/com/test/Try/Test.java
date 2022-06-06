fe52345003fad9877799e3f0663a72b435c10b89
Abarna <36993581+Abarnachan@users.noreply.github.com>
2022-06-06 11:46:35 +0800

    Update Test.java

--- src/test/java/com/test/Try/Test.java
+++ src/test/java/com/test/Try/Test.java
@@ -7,7 +7,7 @@ public class Test
 public static WebDriver driver;
 public static void createInstance()
 {
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vision\\Documents\\Automation\\ChormeDriver\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", "C:\Users\Vision\Documents\Webdriver\chromedriver.exe");
 driver = new ChromeDriver();
 driver.manage().window().maximize();
 }
